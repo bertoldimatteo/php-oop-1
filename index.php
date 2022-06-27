11b04bc96e7a9d0d879b591965f9f4c6ef88dd16
bertoldimatteo <bertoldi.matteo660@gmail.com>
2022-06-27 15:01:25 +0200

    aggiunto movie a index.php e stampato film su html

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php  
+    require_once __DIR__ . '/movie.php';
 
+    $movies = [
+        new Movie('Star Wars la minaccia fantasma', 'Lucasfilm', '1999'),
+        new Movie('Star Wars la vendetta dei sith', 'Lucasfilm', '2005'),
+        new Movie('Interstellar', 'Syncopy Films', '2014'),
+        new Movie('In Time', 'New Regency, Strike Entertainment', '2011'),
+    ]
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +18,15 @@
     <title>OOP PHP</title>
 </head>
 <body>
-    
+    <h1>Best Movie</h1>
+    <ul>
+        <?php foreach($movies as $movie) { ?>
+        <li>
+            <h3><?php echo $movie->title ?></h3>
+            <h4><?php echo $movie->movieHouse ?></h4>
+            <span><?php echo $movie->year ?></span>
+        </li>
+        <?php } ?>
+    </ul>
 </body>
 </html>
